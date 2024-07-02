e4f085f31740a47b7814d74fe96f7618ee740244
Alex's-Branch <arriaga.91c@gmail.com>
2024-07-01 19:31:36 -0500

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,16 @@
+﻿namespace BookCubAPI
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        //public int YearPublished { get; set; }
+        public DateTime PublicationDate {  get; set; }
+        public int PageCount { get; set; }
+        public string Genre { get; set; }
+        public string CoverImageUrl { get; set; }
+        public List<string> Tags { get; set; }
+        public bool IsRead { get; set; }
+    }
+}
---
+++ Services/BookService.cs
@@ -0,0 +1,13 @@
+﻿namespace BookCubAPI.Services
+{
+    public interface IBookService
+    {
+        Task<List<Book>> SelectAllBooks();
+
+        public class BookService : IBookService
+        {
+            private readonly IBookService _bookService;
+            private string _conn;
+        }
+    }
+}
